bfe7076d1c9896fc47cfc1e18dcbee7a777c1873
Nicholas Sack <nick@imns.dev>
2022-06-12 23:43:52 -0500

    updated to use updated clap crate

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,28 +1,23 @@
-use clap::{App, Arg};
+use clap::Parser;
+
+/// Simple echo program
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Omit newline on print
+    #[clap(short = 'n', long)]
+    no_newline: bool,
+
+    /// Input to echo
+    #[clap(required = true, min_values = 1)]
+    text: Vec<String>,
+}
 
 fn main() {
-    let matches = App::new("echor")
-        .version("0.1.0")
-        .author("Nicholas Sack <njsack@proton.me>")
-        .about("Rust echo")
-        .arg(
-            Arg::with_name("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .min_values(1),
-        )
-        .arg(
-            Arg::with_name("omit_newline")
-                .short("n")
-                .help("Do not print newline")
-                .takes_value(false),
-        )
-        .get_matches();
+    let args = Args::parse();
 
-    let text = matches.values_of_lossy("text").unwrap();
-    let omit_newline = matches.is_present("omit_newline");
+    let text = args.text.join(" ");
+    let omit_newline = args.no_newline;
 
-    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
-
+    print!("{}{}", text, if omit_newline { "" } else { "\n" });
 }
